c4cbcc391b851a47b11a069c9cfc65d28d534553
pilpan <plip2010195@gmail.com>
2022-09-13 17:41:04 +0300

    fix

--- public/test.js
+++ public/test.js
@@ -1,3 +1,7 @@
-function sum(a, b) {
-  return a + b;
-}
+<pre style='height: 10em; background-color: rgba(255, 255, 128, .5);'>
+  <code>
+  function sum(a, b) {
+    return a + b;
+  }  
+  </code>
+  </pre>
