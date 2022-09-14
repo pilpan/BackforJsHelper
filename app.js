27cdb0b6eda8554db177ca98d4f022ca8806484f
pilpan <plip2010195@gmail.com>
2022-09-14 18:05:38 +0300

    fix

--- app.js
+++ app.js
@@ -165,7 +165,7 @@ app.post('/posts/:id', async (req, res) => {
   }
 });
 
-app.get('/catque', (req,res) => {
+app.post('/catque', (req,res) => {
   const data = fs.readFileSync(`./public${req.body.path}`,'utf-8');
   res.json(data);
 })
