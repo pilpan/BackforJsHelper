14807405a1ca809eac8e38c874b212e920c0e1d1
pilpan <plip2010195@gmail.com>
2022-09-15 10:44:53 +0300

    fix

--- app.js
+++ app.js
@@ -146,7 +146,7 @@ app.get('/stat/:num/:id', async (req, res) => {
       }
       await temp.save();
     }
-    res.json({id: temp.id, email: temp.email, exp: temp.exp, userName: temp.userName});
+    res.json({id: temp.id, email: temp.email, exp: temp.exp, userName: temp.userName, avatar: temp.avatar});
   } catch (error) {
     console.log(error);
     res.json(error);
