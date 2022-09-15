c0e73f6db470a76e65760aa31a31fbfc5be0167f
pilpan <plip2010195@gmail.com>
2022-09-15 10:34:05 +0300

    fix

--- db/seeders/20220912123328-addCat.js
+++ db/seeders/20220912123328-addCat.js
@@ -4,22 +4,22 @@ module.exports = {
   async up (queryInterface, Sequelize) {
      await queryInterface.bulkInsert('Categories', [{
        name: 'Junior',
-       price: 1,
+       price: 10,
        createdAt: new Date(),
        updatedAt: new Date(),
      },{
       name: 'Middle',
-      price: 10,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    },{
-      name: 'Senior',
       price: 100,
       createdAt: new Date(),
       updatedAt: new Date(),
     },{
-      name: 'Rev',
+      name: 'Senior',
       price: 500,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },{
+      name: 'Rev',
+      price: 0,
       createdAt: new Date(),
       updatedAt: new Date(),
     }], {});
